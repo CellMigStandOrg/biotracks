cfc5a6fc829d40aec92679830db92503260f2490
jmoore <josh@glencoesoftware.com>
2017-01-28 11:28:02 -0600

    Move from global to instance variables

--- tests/test_readfile.py
+++ tests/test_readfile.py
@@ -16,12 +16,11 @@ class ReadFileTest(unittest.TestCase):
     def setUp(self):
         """ Setting up for the test """
         print("ReadFileTest:setUp_:begin")
-        global f
-        global n
-        global joint_id
-        f = "test_files/tracks_ctr.csv"
-        n = 20
-        joint_id = 'Track N'
+        self.grouped = []
+        self.read_file = None
+        self.f = "test_files/tracks_ctr.csv"
+        self.n = 20
+        self.joint_id = 'Track N'
         print("ReadFileTest:setUp_:end")
 
     # ending the test
@@ -35,25 +34,24 @@ class ReadFileTest(unittest.TestCase):
     def test_01_import_file(self):
         """Test routine import_file"""
         print("ReadFileTest:test_01_import_file")
-        self.assertTrue(os.path.exists(f))
-        global read_file
-        read_file = readfile.import_file(f, n)
-        print(read_file)
-        assert read_file is not None
+        self.assertTrue(os.path.exists(self.f))
+        self.read_file = readfile.import_file(self.f, self.n)
+        print(self.read_file)
+        assert self.read_file is not None
 
     # test routine group_by_joint_id
     def test_02_group_by_joint_id(self):
         """Test routine group_by_joint_id"""
-        global grouped
-        grouped = readfile.group_by_joint_id(read_file, joint_id)
-        self.assertTrue(grouped is not None)
-        self.assertIsInstance(grouped, pd.core.groupby.DataFrameGroupBy)
+        self.grouped = readfile.group_by_joint_id(
+            self.read_file, self.joint_id)
+        self.assertTrue(self.grouped is not None)
+        self.assertIsInstance(self.grouped, pd.core.groupby.DataFrameGroupBy)
         print("ReadFileTest:test_02_group_by_joint_id")
 
     # test routine split_in_objs_evnts
     def test_03_split_in_objs_evnts(self):
         """Test routine split_in_objs_evnts"""
-        dict_ = readfile.split_in_objs_evnts(joint_id, grouped)
+        dict_ = readfile.split_in_objs_evnts(self.joint_id, self.grouped)
         self.assertTrue(dict_ is not None)
         for key in dict_:
             print("key: %s , value: %s" % (key, dict_[key]))
