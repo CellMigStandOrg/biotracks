bfddc1c34d2a9ba9765ce12d35736da863f7a685
simleo <simleo@crs4.it>
2017-05-08 16:20:35 +0100

    test_readfile: cleanup

--- tests/test_readfile.py
+++ tests/test_readfile.py
@@ -1,8 +1,4 @@
-# -*- coding: utf-8 -*-
 import os
-import sys
-# needed if using pytest (not needed for py.test)
-sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../biotracks")
 
 from biotracks import readfile
 from pytest import fixture
@@ -12,11 +8,9 @@ import pandas as pd
 class Data(object):
 
     def __init__(self):
-        self.grouped = []
         self.read_file = None
         directory = os.path.dirname(__file__)
         self.f = os.path.join(directory, "test_files", "test_fake_tracks_tm.xml")
-        self.joint_id = 'Track N'
 
 
 @fixture(scope="class")
@@ -25,15 +19,11 @@ def data():
 
 
 class TestReadFile(object):
-    """Read file test."""
 
     def test_01_import_file(self, data):
-        """Test routine read_file"""
         exp_keys = set(['objects', 'links'])
-        print("ReadFileTest:test_01_read_file")
         assert os.path.exists(data.f)
         data.read_file = readfile.read_file(data.f, {})
-        print(data.read_file)
         assert data.read_file is not None
         assert set(data.read_file) == exp_keys
         for k in exp_keys:
