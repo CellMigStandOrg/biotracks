c7195d1875946b7fa0ed93b875e04cea1caf2039
simleo <simleo@crs4.it>
2017-05-19 17:04:27 +0100

    pushtopandas: use logging

--- biotracks/pushtopandas.py
+++ biotracks/pushtopandas.py
@@ -6,33 +6,30 @@ except ImportError:
     from datapackage.pushpull import _convert_path as convert_path
 import pandas as pd
 
+from .utils import NullLogger
 from .names import OBJECTS_TABLE_NAME, LINKS_TABLE_NAME
 
 
-def push_to_pandas(directory, object_id_cmso):
-    """Push the datapackage to a pandas storage.
+def push_to_pandas(directory, object_id_cmso, logger=None):
+    """Push a datapackage to a pandas storage.
 
-    Keyword arguments:
-    directory -- the directory to look into for the json descriptor
-    object_id_cmso -- the join_id specified in the .ini file
+    arguments:
+    directory -- the datapackage directory containing the json file
+    object_id_cmso -- the object id
     """
+    if logger is None:
+        logger = NullLogger()
     descr = directory + os.sep + 'dp.json'
     storage = dp.push_datapackage(descriptor=descr, backend='pandas')
-    print(storage.buckets)
-
     objects = storage[convert_path("objects.csv", OBJECTS_TABLE_NAME)]
     links = storage[convert_path("links.csv", LINKS_TABLE_NAME)]
-
     objects.reset_index(inplace=True)
-    print(objects.head()), print(links.head())
 
     # aggregation to construct tracks
     tracks_dict = {}
     list_ = []
     TRACK_ID = -1
-
     for link in links.LINK_ID.unique():
-
         tmp = links[links.LINK_ID == link]
         rest = links[(links.LINK_ID != link) & (~links.LINK_ID.isin(list_))]
         ind = rest[object_id_cmso].isin(tmp[object_id_cmso])
@@ -42,7 +39,6 @@ def push_to_pandas(directory, object_id_cmso):
             if link not in [l for v in tracks_dict.values() for l in v]:
                 TRACK_ID += 1
                 tracks_dict[TRACK_ID] = [link]
-
         # shared spots
         if any(ind):
             for index, b in enumerate(ind):
@@ -53,7 +49,6 @@ def push_to_pandas(directory, object_id_cmso):
                         tracks_dict[TRACK_ID] = []
                     tracks_dict[TRACK_ID].append(link)
                     tracks_dict[TRACK_ID].append(rest.iloc[index].LINK_ID)
-
         list_.append(link)
 
     # get unique links and construct tracks dataframe
@@ -61,12 +56,10 @@ def push_to_pandas(directory, object_id_cmso):
     for key, value in tracks_dict.items():
         unique_set = set(value)
         tracks_dict_unique[key] = unique_set
-    print('>>> Created {} tracks'.format(len(tracks_dict_unique)))
-
+    logger.debug('Created %d tracks', len(tracks_dict_unique))
     tracks = pd.DataFrame()
     for key, value in tracks_dict_unique.items():
         for link in value:
             tracks = tracks.append([[key, link]], ignore_index=True)
     tracks.columns = ['TRACK_ID', 'LINK_ID']
-
-    return {'objects' : objects, 'links' : links, 'tracks' : tracks}
+    return {'objects': objects, 'links': links, 'tracks': tracks}
