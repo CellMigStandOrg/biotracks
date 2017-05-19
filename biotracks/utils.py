bd3dac058c0b66549c7aae649dad7b9d704bb486
simleo <simleo@crs4.it>
2017-05-19 16:02:12 +0100

    readfile: refactor readers; use logging

---
+++ biotracks/utils.py
@@ -0,0 +1,13 @@
+import logging
+
+
+class NullHandler(logging.Handler):
+    def emit(self, record):
+        pass
+
+
+class NullLogger(logging.Logger):
+    def __init__(self):
+        logging.Logger.__init__(self, "null")
+        self.propagate = 0
+        self.handlers = [NullHandler()]
