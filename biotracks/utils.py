42ae961401414ad2d7167ee851a721520b8c07ea
simleo <simleo@crs4.it>
2017-05-22 14:52:40 +0100

    create_dpkg: use logging; cleanup

--- biotracks/utils.py
+++ biotracks/utils.py
@@ -1,6 +1,42 @@
 import logging
 
 
+LOG_LEVELS = frozenset([
+    'CRITICAL',
+    'DEBUG',
+    'ERROR',
+    'FATAL',
+    'INFO',
+    'NOTSET',
+    'WARN',
+    'WARNING',
+])
+LOG_FORMAT = '%(asctime)s %(levelname)s: [%(name)s] %(message)s'
+
+
+def get_log_level(s):
+    try:
+        return int(s)
+    except ValueError:
+        level_name = s.upper()
+        if level_name in LOG_LEVELS:
+            return getattr(logging, level_name)
+        else:
+            raise ValueError('%r is not a valid log level' % (s,))
+
+
+def get_logger(name, level='INFO', f=None, mode='a'):
+    logger = logging.getLogger(name)
+    logger.setLevel(get_log_level(level))
+    if isinstance(f, str):
+        handler = logging.FileHandler(f, mode=mode)
+    else:
+        handler = logging.StreamHandler(f)
+    handler.setFormatter(logging.Formatter(LOG_FORMAT))
+    logger.addHandler(handler)
+    return logger
+
+
 class NullHandler(logging.Handler):
     def emit(self, record):
         pass
