a4ee280e1c11d3266dc4bd4ac1bc92eae6e9add1
Gwendolien Sergeant <gwendolien.sergeant@ugent.be>
2017-11-22 16:01:20 +0100

    formatting

--- biotracks/readfile.py
+++ biotracks/readfile.py
@@ -44,6 +44,7 @@ class AbstractReader(metaclass=ABCMeta):
     """\
     Common interface for all tracking data readers.
     """
+
     def __init__(self, fname, conf=None, log_level=None):
         """\
         Set the input file name for the reader.
@@ -303,10 +304,10 @@ class CellProfilerReader(AbstractReader):
         for index, row in cp_df.iterrows():
             objects_dict[index] = [row[self.frame], row[self.x], row[self.y]]
         objects_df = pd.DataFrame(
-                [[key, value[0], value[1], value[2]]
-                 for key, value in objects_dict.items()],
-                columns=[self.obj_id, self.frame, self.x, self.y]
-            )
+            [[key, value[0], value[1], value[2]]
+             for key, value in objects_dict.items()],
+            columns=[self.obj_id, self.frame, self.x, self.y]
+        )
         objects_df.columns = [
             cmso.OBJECT_ID, cmso.FRAME_ID, cmso.X_COORD, cmso.Y_COORD
         ]
@@ -394,24 +395,26 @@ class CellmiaReader(AbstractReader):
 
 
 class MosaicReader(AbstractReader):
-    
+
     MOSAIC_COLS = ["Trajectory", "Frame", "x", "y", "z"]
 
     def read(self):
         mo_df = pd.read_csv(self.fname, sep=None, usecols=self.MOSAIC_COLS)
         mo_df.reset_index(inplace=True)
-        cols = [cmso.OBJECT_ID, cmso.LINK_ID, cmso.FRAME_ID, cmso.X_COORD, cmso.Y_COORD, cmso.Z_COORD]
+        cols = [cmso.OBJECT_ID, cmso.LINK_ID, cmso.FRAME_ID,
+                cmso.X_COORD, cmso.Y_COORD, cmso.Z_COORD]
         mo_df.columns = cols
         self._objects = mo_df.drop(cmso.LINK_ID, 1)
-        self._links = mo_df.drop([cmso.FRAME_ID, cmso.X_COORD, cmso.Y_COORD, cmso.Z_COORD], 1)
+        self._links = mo_df.drop(
+            [cmso.FRAME_ID, cmso.X_COORD, cmso.Y_COORD, cmso.Z_COORD], 1)
         self._links[cmso.LINK_ID] -= 1
-
 
 
 class TracksReader(object):
     """\
     Generic reader that delegates to specific ones based on file extension.
     """
+
     def __init__(self, fname, conf=None, log_level=None):
         """\
         Initialize and store a specific reader based on filename extension.
@@ -427,23 +430,23 @@ class TracksReader(object):
                 fname, conf=conf, log_level=log_level
             )
         elif ext == ('.csv' or '.tsv' or '.txt'):
-            #read header and delegate to correct reader
-            #this pandas method can read csv, tsv and txt files
+            # read header and delegate to correct reader
+            # this pandas method can read csv, tsv and txt files
             df = pd.read_csv(fname, header=None, sep=None, nrows=1)
             trackname = df.iloc[0, 0]
             if trackname == 'ImageNumber':
                 self.reader = CellProfilerReader(
-                fname, conf=conf, log_level=log_level
-            )
+                    fname, conf=conf, log_level=log_level
+                )
             elif trackname == 'ID of track':
                 self.reader = CellmiaReader(
-                fname, conf=conf, log_level=log_level
-            )
+                    fname, conf=conf, log_level=log_level
+                )
             elif trackname == 'Trajectory':
                 self.reader = MosaicReader(
-                fname, conf=conf, log_level=log_level
-            )
-            
+                    fname, conf=conf, log_level=log_level
+                )
+
         elif ext == '.json':
             self.reader = BiotracksReader(
                 fname, conf=conf, log_level=log_level
